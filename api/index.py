200601a3eb876742c0cdfdc0253717cdf2ed4e8f
Jayytest <rizzlerohioaigna@gmail.com>
2025-05-19 17:30:01 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -113,18 +113,8 @@ def consume_oculus_iap():
     if response.json().get("success"):
         return jsonify({"result": True})
     return jsonify({"error": True})
-@app.route("/api/td", methods=["POST", "GET"])
-def title_data():
-    response = requests.post(
-        url=f"https://{settings.TitleId}.playfabapi.com/Server/GetTitleData",
-        headers=settings.get_auth_headers()
-    )
 
-    if response.status_code == 200:
-        return jsonify(response.json().get("data").get("Data"))
-    else:
-        return jsonify({}), response.status_code
- 
+
 @app.route("/api/photon", methods=["POST"])
 def photonauth():
     AA = request.get_json()
