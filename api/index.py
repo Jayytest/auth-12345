71c4486ed05883732c845b025e554cfaf9286bb3
Jayytest <rizzlerohioaigna@gmail.com>
2025-05-19 17:28:28 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -113,16 +113,18 @@ def consume_oculus_iap():
     if response.json().get("success"):
         return jsonify({"result": True})
     return jsonify({"error": True})
+@app.route("/api/td", methods=["POST", "GET"])
+def title_data():
+    response = requests.post(
+        url=f"https://{settings.TitleId}.playfabapi.com/Server/GetTitleData",
+        headers=settings.get_auth_headers()
+    )
 
-@app.route("/api/td", methods = ['POST','GET'])
-def pftd():
-    A = "https://{Title}.playfabapi.com/Server/GetTitleData"
-    B = {"X-SecretKey": Dash, "Content-Type": "application/json"}
-    C = requests.post(url=A, headers=B)
-    TD = C.json().get("data", "").get("Data", "")
-
-    return jsonify
-
+    if response.status_code == 200:
+        return jsonify(response.json().get("data").get("Data"))
+    else:
+        return jsonify({}), response.status_code
+ 
 @app.route("/api/photon", methods=["POST"])
 def photonauth():
     AA = request.get_json()
